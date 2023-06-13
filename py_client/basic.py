0f6812cf39dad72fbf804db33b6d0e192c4450cf
vova <vova.bekar7@gmail.com>
2023-06-13 21:52:22 +0300

    set up project

---
+++ py_client/basic.py
@@ -0,0 +1,12 @@
+import requests
+
+
+# endpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/anythoing"
+
+get_response = requests.get(endpoint, json={"query": "Hello world"})  # API -> Method
+# print(get_response.text)
+
+print(get_response.json())
+print(get_response.status_code)
+
