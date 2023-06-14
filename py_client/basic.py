0f6c19d6c444ea9355414485eecaf8584f651627
vova <vova.bekar7@gmail.com>
2023-06-14 18:47:19 +0300

    api app

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,13 +2,16 @@ import requests
 
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anythoing"
+# endpoint = "https://httpbin.org/anythoing"
+endpoint = "http://localhost:8000/api/"  # http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, json={"query": "Hello world"})  # API -> Method
-# print(get_response.text)
-
-print(get_response.json())
+print(get_response.text)
 print(get_response.status_code)
+
+
+print(get_response.json()['message'])
+# print(get_response.status_code)
 
 
 
