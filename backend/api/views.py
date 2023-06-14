9036ac0301178e38aafb77df220132b7648bce13
vova <vova.bekar7@gmail.com>
2023-06-14 22:19:17 +0300

    product app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,18 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    body = request.body  # byte string of JSON data
-    data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    model_data = Product.objects.all().order_by("?").first()
+    data = {
+    }
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        # model instance
+        # serialization
     return JsonResponse(data)
+
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 # endpoint = "https://httpbin.org/anythoing"
 endpoint = "http://localhost:8000/api/"  # http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})  # API -> Method
+get_response = requests.get(endpoint, json={"query": "Hello world"})  # API -> Method
 # print(get_response.text)
 # print(get_response.status_code)
 
