9dd9ce510a16a251686a1228e41e3ce5ffabffd2
vova <vova.bekar7@gmail.com>
2023-06-14 21:51:14 +0300

    get data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,16 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": 'Hi there, this is DJANGO API RESPONSE!!'})
+    body = request.body  # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
