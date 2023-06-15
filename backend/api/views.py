38c864d572cb2bcc1afedd58241e49283314587b
vova <vova.bekar7@gmail.com>
2023-06-15 15:40:23 +0300

    DRF Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,22 @@
-import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    :param request:
+    :param args:
+    :param kwargs:
+    :return:
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {
     }
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        # model instance
-        # serialization
-    return JsonResponse(data)
-
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,12 +6,12 @@ import requests
 endpoint = "http://localhost:8000/api/"  # http://127.0.0.1:8000/
 
 get_response = requests.get(endpoint, json={"query": "Hello world"})  # API -> Method
-# print(get_response.text)
+print(get_response.text)
 # print(get_response.status_code)
 
 
 # print(get_response.json()['message'])
-print(get_response.json())
+# print(get_response.json())
 # print(get_response.status_code)
 
 
